d7c5e1da071fc04368e40889139acf239a860183
unknown <ToO_HAGENSV22@5CG2287KJC.gcc.edu>
2026-08-15 10:15:46 -0400

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>My name is Samuel Hagen (HagenSV). I graduated Cum Laude from Grove City College in 2026, with a major in Computer Science and minors in Mobile Development and Cybersecurity.</p>
+      <p>I am a programming enthusiast. I love to create things and push my skills.</p>
+    </div>
+  );
+}
