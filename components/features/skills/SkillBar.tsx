fa4801dbc289b76ecc8b06d40114a950d7a2df93
HagenSV <samuel@hagensv.dev>
2026-08-16 13:51:57 -0400

    improve skill bar styling

--- components/features/skills/SkillBar.tsx
+++ components/features/skills/SkillBar.tsx
@@ -11,8 +11,8 @@ export default function SkillBar({ skill }: Props ){
 
     return <div>
         <p>{skill.name}</p>
-        <div className="bg-black rounded text-right text-sm">
-            <div className={`bg-white text-black rounded pr-2`} style={{ width: prof }}>
+        <div className="bg-zinc-700 rounded-xl text-right text-sm">
+            <div className={`bg-green-700 dark:bg-green-900 text-white rounded-xl pr-4`} style={{ width: prof }}>
                 <span>{ getProficiencyLabel(skill.proficiency) }</span>
             </div>
         </div>
