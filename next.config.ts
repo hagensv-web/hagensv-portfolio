f0c522285c49f103331c7432be45e095cbe0dcd3
HagenSV <samuel@hagensv.dev>
2026-08-16 10:47:50 -0400

    configure static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '', // Replace with your GitHub repo name
+  assetPrefix: '',
+  distDir: 'out',
+  reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+    useLightningcss: true
+  },
 };
 
 export default nextConfig;
